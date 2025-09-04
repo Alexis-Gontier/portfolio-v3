56cc204b35c1548540de8a30496940c1d03396a9
gontier <alexis.gontier03@laposte.net>
2025-09-04 16:23:40 +0200

    💄 enhance loading and not-found components with improved UI and functionality

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+import { Button } from "@/components/shadcn-ui/button";
+
 export default function NotFound() {
-  return (
-    <div>Not Found</div>
-  )
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-background px-4">
+            <h1 className="text-center text-2xl font-bold text-foreground md:text-4xl">404 - Page Not Found</h1>
+            <p className="text-center text-muted-foreground text-sm md:text-base">
+                Désolé, la page que vous recherchez n&apos;existe pas.
+            </p>
+            <Button asChild>
+                <Link href="/">
+                    Retour à l&apos;accueil
+                </Link>
+            </Button>
+        </div>
+    );
 }
