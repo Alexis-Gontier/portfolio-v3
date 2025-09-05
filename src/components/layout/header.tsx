70c3ffa4e38f02b6452b764f632b2ab4459ff439
Gontier <alexis.gontier03@gmail.com>
2025-09-06 00:34:22 +0200

    ✨ ajouter le composant Header avec une structure de navigation et des éléments interactifs

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import Link from "next/link";
+import { AnimatedThemeToggler } from "@/components/magicui/animated-theme-toggler";
+import { Icon } from "../ui/banner";
 
 export default function Header() {
+
   return (
-    <div>Header</div>
+    <header className="sticky top-0 z-100 w-full h-16 border-b bg-background/30 backdrop-blur">
+      <nav className="relative h-full max-w-3xl mx-auto px-6 border-x bg-background/30 backdrop-blur flex justify-between items-center">
+        <Icon positionX="left" positionY="bottom" />
+        <Icon positionX="right" positionY="bottom" />
+        <Link href="/" className="text-lg font-bold">
+          Algont-ier
+        </Link>
+        <AnimatedThemeToggler />
+      </nav>
+    </header>
   )
 }
