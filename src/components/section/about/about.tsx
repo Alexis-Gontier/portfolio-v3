67265f005418816a16a04f059971d67ec68f5976
Gontier <alexis.gontier03@gmail.com>
2025-09-06 00:34:14 +0200

    ✨ ajouter le composant About avec une structure de base

---
+++ src/components/section/about/about.tsx
@@ -0,0 +1,14 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardTitle
+} from "@/components/shadcn-ui/card";
+
+export default function About() {
+  return (
+    <div id="about" className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      About
+    </div>
+  )
+}
