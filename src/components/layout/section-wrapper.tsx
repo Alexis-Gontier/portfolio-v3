b05961fb284b398f25754fef16e534912a248b71
Gontier <alexis.gontier03@gmail.com>
2025-09-06 00:34:18 +0200

    ✨ refactor le composant SectionWrapper pour accepter une classe personnalisée et simplifier la structure

--- src/components/layout/section-wrapper.tsx
+++ src/components/layout/section-wrapper.tsx
@@ -1,44 +1,12 @@
-import { Plus } from "lucide-react"
-
 type SectionWrapperProps = {
   children?: React.ReactNode
+  className?: string
 }
 
-export default function SectionWrapper({ children }: SectionWrapperProps) {
+export default function SectionWrapper({ children, className }: SectionWrapperProps) {
   return (
-    <section className="border-y px-4">
-      <div className="relative max-w-xl mx-auto min-h-20 p-8 border-x">
-        <Icon positionX="left" positionY="top" />
-        <Icon positionX="right" positionY="top" />
-        <Icon positionX="left" positionY="bottom" />
-        <Icon positionX="right" positionY="bottom" />
-        {children}
-      </div>
+    <section className={`max-w-3xl mx-auto p-4 md:p-8 border-x ${className}`}>
+      {children}
     </section>
-  )
-}
-
-function Icon({
-  positionX = "right",
-  positionY = "bottom"
-}: {
-  positionX?: "left" | "right";
-  positionY?: "top" | "bottom";
-} = {}) {
-  const getPositionClasses = () => {
-    const positions = {
-      'left-top': 'absolute -top-3.5 -left-3.5',
-      'right-top': 'absolute -top-3.5 -right-3.5',
-      'left-bottom': 'absolute -bottom-3.5 -left-3.5',
-      'right-bottom': 'absolute -bottom-3.5 -right-3.5'
-    }
-
-    return positions[`${positionX}-${positionY}` as keyof typeof positions]
-  }
-
-  return (
-    <Plus
-      className={`${getPositionClasses()} text-border size-7 bg-background p-px`}
-    />
   )
 }
